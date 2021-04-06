4986294037946fcb97041968f523c77fa1e1dd9d
Kacper Rozdolski <k.rpzdolski@gmail.com>
2021-04-06 13:15:52 +0200

    Basic functionality. Prevent from sedning multiply requests.

--- src/components/LyteApp.vue
+++ src/components/LyteApp.vue
@@ -1,85 +1,86 @@
 <template>
   <div class="lyte-body">
     <section class="lyte-container">
-      <div class="lyte-wrapper">
-        <div class="lyte-app">
+      <div class="lyte-app">
+        <div class="lyte-description">
           <h1>Lyte links, big results</h1>
           <h2>
             A URL shortener built meant to help you grow and protect your brand.
           </h2>
-          <div class="lyte-input">
-            <input
-              type="text"
-              v-model="url"
-              @keydown.enter="console"
-              placeholder="Shorten your links"
-            />
-            <button type="submit" @click="console">Shorten</button>
-          </div>
-          <div class="lyte-link-container">
-            <div class="link-table">
-              <component
-                :is="'LyteLink'"
-                v-for="link in links"
-                :key="link.id"
-                :id="link.id"
-                :link="link.url"
-                @deleteLink="deleteLink"
-                @copyLink="copyLink"
-              ></component>
-            </div>
-            <hr />
-            <p>1</p>
-          </div>
+        </div>
+        <div class="lyte-input">
+          <input
+            type="text"
+            v-model="url"
+            @keydown.enter="createLink"
+            placeholder="Shorten your links"
+          />
+          <button type="submit" @click="createLink">Shorten</button>
+        </div>
+        <div class="lyte-link-table">
+          <component
+            :is="'LyteLink'"
+            v-for="link in links"
+            :key="link.id"
+            :id="link.id"
+            :link="link.url"
+            @deleteLink="deleteLink"
+            @copyLink="copyLink"
+          ></component>
         </div>
       </div>
-      <footer class="lyte-footer">
-        <p>copyright © 2021 kacper rozdolski. all rights reserved.</p>
-      </footer>
-    </section>
-    <section class="playground">
-      <LinkTable />
+      <footer></footer>
     </section>
   </div>
 </template>
 
 <script>
-import LinkTable from "./LinkTable.vue";
 import LyteLink from "./LyteLink.vue";
 export default {
   name: "HelloWorld",
-  components: { LyteLink, LinkTable },
+  components: { LyteLink },
   data() {
     return {
       url: "",
       links: [],
       id: 0,
+      isLoading: false,
     };
   },
   methods: {
-    console() {
-      let linkRequest = {
-        destination: this.url,
-        domain: { fullName: "lyte.1amcode.com" },
-      };
-      let requestHeaders = {
-        "Content-Type": "application/json",
-        apikey: "ec39449312484443818788162831118e",
-      };
-      this.axios({
-        method: "post",
-        url: "https://api.rebrandly.com/v1/links",
-        data: JSON.stringify(linkRequest),
-        headers: requestHeaders,
-        dataType: "json",
-      }).then((response) => {
-        this.links.push({
-          id: this.id++,
-          url: response.data.shortUrl,
-        });
-        document.querySelector("input").value = "asdadssad";
-        this.url = "";
-      });
+    createLink() {
+      if (this.isLoading == false) {
+        let linkRequest = {
+          destination: this.url,
+          domain: { fullName: "lyte.1amcode.com" },
+        };
+        let requestHeaders = {
+          "Content-Type": "application/json",
+          apikey: "ec39449312484443818788162831118e",
+        };
+        this.isLoading = true;
+        console.log("Wysłam", "Ładuje się? " + this.isLoading);
+        this.axios({
+          method: "post",
+          url: "https://api.rebrandly.com/v1/links",
+          data: JSON.stringify(linkRequest),
+          headers: requestHeaders,
+          dataType: "json",
+        })
+          .then((response) => {
+            this.links.push({
+              id: this.id++,
+              url: response.data.shortUrl,
+            });
+            this.isLoading = false;
+            console.log("Odebrałem", "Ładuje się? " + this.isLoading);
+            this.url = "";
+          })
+          .catch(() => {
+            this.isLoading = false;
+            console.log("Odebrałem", "Ładuje się? " + this.isLoading);
+          });
+      }
     },
     copyLink(id) {
       let link = this.links.find((link) => link.id == id);
@@ -98,10 +99,6 @@ export default {
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;800&display=swap");
 
-.playground {
-  width: 100%;
-  height: 100vh;
-}
 .lyte-body {
   min-height: 100vh;
   width: 100%;
@@ -109,19 +106,16 @@ export default {
   display: flex;
   flex-direction: column;
   .lyte-container {
-    height: 100vh;
-    width: 100vw;
+    min-height: 100vh;
+    width: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
-    .lyte-wrapper {
-      width: 100%;
-      height: 100%;
+    .lyte-app {
       display: flex;
-      align-items: center;
       flex-direction: column;
-      .lyte-app {
-        height: 75%;
+      min-height: 90vh;
+      .lyte-description {
         h1 {
           font-size: 4.5rem;
           margin: 40px 0;
@@ -132,57 +126,39 @@ export default {
           margin: 40px 0;
           font-weight: 100;
         }
-        .lyte-input {
-          input {
-            width: 78%;
-            height: 40px;
-            margin: 0;
-            padding: 0 0 0 2%;
-            border: 0;
-            outline: 0;
-          }
-          button {
-            width: 20%;
-            height: 40px;
-            padding: 0;
-            margin: 0;
-            border: 0;
-            outline: 0;
-            cursor: pointer;
-          }
-          button:hover {
-            background: rgb(206, 206, 206);
-          }
+      }
+      .lyte-input {
+        input {
+          width: 78%;
+          height: 40px;
+          margin: 0;
+          padding: 0 0 0 2%;
+          border: 0;
+          outline: 0;
         }
-        .lyte-link-container {
-          height: 74%;
-          .link-table {
-            height: 280px;
-          }
-          hr {
-            border: 0;
-            height: 2px;
-            background: rgb(255, 255, 255);
-          }
+        button {
+          width: 20%;
+          height: 40px;
+          padding: 0;
+          margin: 0;
+          border: 0;
+          outline: 0;
+          cursor: pointer;
+        }
+        button:hover {
+          background: rgb(206, 206, 206);
         }
       }
+      .lyte-link-table {
+        color: white;
+      }
     }
-    .lyte-footer {
+    footer {
+      height: 10vh;
       width: 100%;
-      height: 10%;
+      background: red;
       align-self: flex-end;
-      display: flex;
-      align-items: center;
-      justify-content: center;
     }
-  }
-  .lyte-sidebar {
-    height: 100vh;
-    width: 30vw;
-    background: white;
-    display: flex;
-    align-items: center;
-    justify-content: center;
   }
 }
 </style>
